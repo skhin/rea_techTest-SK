995fd0b0716b31f73da1e70aaa743d3e7e582c89
skhin <88125976+skhin@users.noreply.github.com>
2022-03-20 16:42:58 +0800

    Checkout page with stripe

--- rea_vue/src/router/index.js
+++ rea_vue/src/router/index.js
@@ -7,6 +7,7 @@ import Cart from "../views/Cart.vue";
 import SignUp from "../views/SignUp.vue";
 import LogIn from "../views/LogIn.vue";
 import MyAccount from "../views/MyAccount.vue";
+import Checkout from "../views/Checkout.vue";
 import store from "@/store";
 
 const routes = [
@@ -39,6 +40,15 @@ const routes = [
     path: "/my-account",
     name: "MyAccount",
     component: MyAccount,
+    meta: {
+      requireLogin: true,
+    },
+  },
+
+  {
+    path: "/cart/checkout",
+    name: "Checkout",
+    component: Checkout,
     meta: {
       requireLogin: true,
     },
